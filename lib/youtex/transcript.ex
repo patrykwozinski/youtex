e22f05ff89ba59e68e5e15a491f41e944e1c0683
Patryk Woziński <patryk.wozinski@gmail.com>
2021-03-11 12:43:14 +0100

    struct def

--- lib/youtex/transcript.ex
+++ lib/youtex/transcript.ex
@@ -1,3 +1,6 @@
 defmodule Youtex.Transcript do
     @moduledoc false
+
+    @enforce_keys [:video_id, :url, :name, :language_code, :generated, :translatable]
+    defstruct [:video_id, :url, :name, :language_code, :generated, :translatable]
 end
