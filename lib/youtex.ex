66a353c58188b78dae9a127922cbead584b4ecbe
Patryk Woziński <patryk.wozinski@gmail.com>
2021-03-11 09:36:44 +0100

    init

---
+++ lib/youtex.ex
@@ -0,0 +1,18 @@
+defmodule Youtex do
+  @moduledoc """
+  Documentation for `Youtex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Youtex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/youtex_test.exs
@@ -0,0 +1,8 @@
+defmodule YoutexTest do
+  use ExUnit.Case
+  doctest Youtex
+
+  test "greets the world" do
+    assert Youtex.hello() == :world
+  end
+end
