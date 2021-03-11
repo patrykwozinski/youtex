bdfdedf7073b60f5a1346ae5717cddbd442e9603
Patryk Woziński <patryk.wozinski@gmail.com>
2021-03-11 10:33:30 +0100

    types

--- lib/youtex.ex
+++ lib/youtex.ex
@@ -1,10 +1,14 @@
 defmodule Youtex do
   @moduledoc false
 
+  @type video_id :: integer
+
+  @spec list_transcripts(video_id)
   def list_transcripts(_video_id) do
     :ok
   end
 
+  @spec get_transcription(video_id)
   def get_transcription(_video_id) do
     :ok
   end
