9d14e6c753a0fc262b86d64df3b80777fbf34761
Patryk Woziński <patryk.wozinski@gmail.com>
2021-03-11 12:46:43 +0100

    type

--- lib/youtex/transcript.ex
+++ lib/youtex/transcript.ex
@@ -3,4 +3,13 @@ defmodule Youtex.Transcript do
 
     @enforce_keys [:video_id, :url, :name, :language_code, :generated, :translatable]
     defstruct [:video_id, :url, :name, :language_code, :generated, :translatable]
+
+    @type t :: %__MODULE__{
+      required(:video_id) => String.t(),
+      required(:url) => String.t(),
+      required(:name) => String.t(),
+      required(:language_code) => String.t(),
+      required(:generated) => boolean,
+      required(:translatable) => boolean
+    }
 end
