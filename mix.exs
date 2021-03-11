bb773b9202b1af228f6af8ff2c455460fcd64620
Patryk Woziński <patryk.wozinski@gmail.com>
2021-03-11 13:59:58 +0100

    run poison

--- mix.exs
+++ mix.exs
@@ -14,7 +14,8 @@ defmodule Youtex.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      applications: [:elixir_xml_to_map],
+      extra_applications: [:logger, :poison, :httpoison]
     ]
   end
 
